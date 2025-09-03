7e596a5c27427e0bf9b9f8cfb3a8f1d7ae38e494
Artem Kravchenko <coo2per@gmail.com>
2025-09-03 21:17:53 +0300

    add mat-icon to the list

--- src/app/admin-page/admin-sidenav/admin-sidenav.ts
+++ src/app/admin-page/admin-sidenav/admin-sidenav.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { NavLink } from '../../core/models/nav-link';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-admin-sidenav',
     imports: [
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        MatIconModule,
     ],
     templateUrl: './admin-sidenav.html',
     styleUrl: './admin-sidenav.scss'
 })
 export class AdminSidenav {
     protected readonly sidenavLinks: NavLink[] = [
-        {label: 'Список покупателей', path: '/admin/users-list', icon: 'patient_list'},
+        {label: 'Список покупателей', path: '/admin/users-list', icon: 'article_person'},
     ];
 }
