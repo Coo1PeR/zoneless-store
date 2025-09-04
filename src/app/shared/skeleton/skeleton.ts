de7e9ad3a43810ee06a6374a5a77adb00eb995d1
Artem Kravchenko <coo2per@gmail.com>
2025-09-04 22:47:10 +0300

    add the skeleton component

---
+++ src/app/shared/skeleton/skeleton.ts
@@ -0,0 +1,22 @@
+import { Component, computed, input } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-skeleton',
+    imports: [
+        NgStyle
+    ],
+  templateUrl: './skeleton.html',
+  styleUrl: './skeleton.scss'
+})
+export class Skeleton {
+    public width = input<string | number>(160);
+    public height = input<string | number>('100%');
+    public radius = input<number>(4);
+
+    style = computed(() => ({
+        width: typeof this.width() === 'number' ? `${this.width()}px` : this.width(),
+        height: typeof this.height() === 'number' ? `${this.height()}px` : this.height(),
+        borderRadius: `${this.radius()}px`,
+    }));
+}
