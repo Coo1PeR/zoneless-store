5385ab679c8d328885b56895da54421d5b6ec28a
Artem Kravchenko <coo2per@gmail.com>
2025-09-08 08:56:26 +0300

    use a mat-list

--- src/app/admin-page/admin-sidenav/admin-sidenav.ts
+++ src/app/admin-page/admin-sidenav/admin-sidenav.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavLink } from '../../core/models/nav-link';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-admin-sidenav',
@@ -9,6 +11,8 @@ import { MatIconModule } from '@angular/material/icon';
         RouterLink,
         RouterLinkActive,
         MatIconModule,
+        MatListModule,
+        MatButtonModule,
     ],
     templateUrl: './admin-sidenav.html',
     styleUrl: './admin-sidenav.scss'
